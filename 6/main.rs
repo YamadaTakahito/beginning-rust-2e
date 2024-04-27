7add0e3f47c8c5aceeb4f7d066c4caf9448e139e
YamadaTakahito <d.p.t.a.d.fighter@gmail.com>
2024-04-27 20:24:25 +0900

    Update variables and operations in main.rs
    The newly created main.rs file now includes the definition of variables with different data types (i16, u32, f32) and their operations. It demonstrates printing the result of summing these variables after casting them to i8. Previous commented-out operation demonstrations with arrays and integers also exist within the file.

---
+++ 6/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    // let a: i8 = 5;
+    // let b: i16 = 5;
+    // let c: i32 = 5;
+    // let d: i64 = 5;
+    // let e: i128 = 5;
+    // println!("{} {} {} {} {}", a, b, c, d, e);
+    //
+    // let arr = [11, 22, 33];
+    // let i: usize = 1;
+    // println!("{}", arr[i]);
+
+    let a: i16 = 12;
+    let b: u32 = 4;
+    let c: f32 = 3.7;
+    println!("{}", a as i8 + b as i8 + c as i8)
+}
